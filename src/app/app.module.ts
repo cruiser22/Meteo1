fe696238a0a9f3872b71cedaca9f935706c10788
kevin <kevingossart59950@gmail.com>
2023-05-25 09:34:26 +0200

    suppression de l'import entête non utilsé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EnTeteComponent } from './components/en-tete/en-tete.component';
 import { MeteoActuelleComponent } from './components/meteo-actuelle/meteo-actuelle.component';
 import { PrevisionsComponent } from './components/previsions/previsions.component';
 import { LocalisationComponent } from './components/localisation/localisation.component';
@@ -11,7 +10,6 @@ import { LocalisationComponent } from './components/localisation/localisation.co
 @NgModule({
   declarations: [
     AppComponent,
-    EnTeteComponent,
     MeteoActuelleComponent,
     PrevisionsComponent,
     LocalisationComponent
