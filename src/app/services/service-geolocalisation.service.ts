e3ade7c5ae22a231fa49e79c6efe5d81cdb021ab
kevin <kevingossart59950@gmail.com>
2023-05-26 12:53:32 +0200

    test intégration geolocalisation dans prévision

--- src/app/services/service-geolocalisation.service.ts
+++ src/app/services/service-geolocalisation.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ServiceGeolocalisationService {
 
+
   getCurrentPosition(): Promise<GeolocationPosition> {
     return new Promise ((resolve, reject) => {
       if (navigator.geolocation) {
@@ -16,9 +17,14 @@ export class ServiceGeolocalisationService {
         reject(error);
       }
     );
+
   } else {
     reject('La géolocalisation n\'est pas prise en charge par votre navigateur');
   }
     });
+  }
+
+  test() {
+    console.log("coucou")
   }
 }
